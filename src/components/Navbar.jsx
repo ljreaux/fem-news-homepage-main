8f12a86fdd56033829b788271cd04dbd23fec55e
Larry Reaux <larryreaux@gmail.com>
2024-03-16 23:59:42 -0500

    set up mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,36 @@
+import { useState } from "react";
 import logo from "../assets/images/logo.svg";
+import open from "../assets/images/icon-menu.svg";
+import close from "../assets/images/icon-menu-close.svg";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(true);
   return (
     <nav className="navbar">
       <img src={logo} alt="logo" className="logo" />
-      <ul className="nav-items">
-        <li>
-          <a href="/home">Home</a>
-        </li>
-        <li>
-          <a href="/new">New</a>
-        </li>
-        <li>
-          <a href="/popular">Popular</a>
-        </li>
-        <li>
-          <a href="/trending">Trending</a>
-        </li>
-        <li>
-          <a href="/categories">Categories</a>
-        </li>
-      </ul>
+      <div>
+        {" "}
+        <button onClick={() => setIsOpen((prev) => !prev)}>
+          <img src={isOpen ? open : close} alt="" />
+        </button>
+        <ul className={`nav-items ${isOpen && "hidden"}`}>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/new">New</a>
+          </li>
+          <li>
+            <a href="/popular">Popular</a>
+          </li>
+          <li>
+            <a href="/trending">Trending</a>
+          </li>
+          <li>
+            <a href="/categories">Categories</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
--- src/components/articles/Main.jsx
+++ src/components/articles/Main.jsx
@@ -1,9 +1,10 @@
 import image3 from "../../assets/images/image-web-3-desktop.jpg";
+import image3mobile from "../../assets/images/image-web-3-mobile.jpg";
 
 export default function Main() {
   return (
     <div className="main-section">
-      <img src={image3} alt="main image" />
+      <img srcSet={`${image3mobile} 600w`} src={image3} alt="main image" />
       <h1 className="heading-color">The Bright Future of Web 3.0?</h1>
       <div>
         <p className="section-color">
