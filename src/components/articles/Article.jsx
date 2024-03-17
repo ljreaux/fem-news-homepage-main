779604301dce4d062999b1fcbed2e8465dadcdeb
Larry Reaux <larryreaux@gmail.com>
2024-03-16 21:38:46 -0500

    finished colors and active states

--- src/components/articles/Article.jsx
+++ src/components/articles/Article.jsx
@@ -6,7 +6,9 @@ export default function Article({ imgSrc, title, content, number }) {
       <img src={imgSrc} alt={"article image"} />
       <div>
         <h2>{number}</h2>
-        <h3>{title}</h3>
+        <h3>
+          <a href="">{title}</a>
+        </h3>
         <p>{content}</p>
       </div>
     </div>
